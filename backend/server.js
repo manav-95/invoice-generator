335ef2ed7a9a29ec4e7c78325b0d37ed53c6634c
manav <manavsuryawanshi1234@gmail.com>
2025-11-22 14:42:04 +0530

    set up for deployment

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,12 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin:[
+        "http://localhost:5173/",
+        ""
+    ]
+}));
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
