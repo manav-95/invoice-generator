751420984fbf7377acc520617c3f8f4261867b29
manav <manavsuryawanshi1234@gmail.com>
2025-11-22 14:56:09 +0530

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(cors({
     origin:[
         "http://localhost:5173/",
-        ""
+        "https://demo-invoice-gen.netlify.app/"
     ]
 }));
 app.use(express.json());
